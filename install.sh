4f1bf475620566b479901d1c0b0ff6686646a930
sandaya <samuel@andaya.net>
2011-12-19 18:16:45 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ln -sfn ~/.vim/vimrc ~/.vimrc
+
+cd ~/.vim
+[ ! -d "tmp" ] && mkdir "tmp"
+cd tmp
+[ ! -d "backup" ] && mkdir "backup"
+[ ! -d "swap" ] && mkdir "swap"
+[ ! -d "undo" ] && mkdir "undo"
+[ ! -d "view" ] && mkdir "view"
+
+
