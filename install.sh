4d5b21842d6a656f06fc95ae22f479ef47981a7c
sandaya <samuel@andaya.net>
2013-10-19 12:07:39 -0400

    updated

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ git pull origin master
 ln -sfn $(pwd)/vimrc ~/.vimrc
 [ ! -d ~/.vim ] && ln -sfn $(pwd) ~/.vim
 
+if [ -d ./bundle/vundle ]; then
+    cd ./bundle/vundle
+    git pull origin master
+else
+    git clone https://github.com/gmarik/vundle.git ./bundle/vundle
+fi
+
 [ ! -d "tmp" ] && mkdir "tmp"
 cd tmp
 [ ! -d "backup" ] && mkdir "backup"
@@ -13,13 +20,6 @@ cd tmp
 [ ! -d "undo" ] && mkdir "undo"
 [ ! -d "view" ] && mkdir "view"
 cd ~
-
-if [ -d ./bundle/vundle ]; then
-    cd ./bundle/vundle
-    git pull origin master
-else
-    git clone https://github.com/gmarik/vundle.git ./bundle/vundle
-fi
 
 vim +BundleInstall +qall
 
